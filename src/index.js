9a1ceb17e93510601379b3c4507b864a6bea93bf
seeduardo <romenflatiron@gmail.com>
2019-01-14 09:56:11 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const handleClick = () => {
+  alert('what a load of dopey noise')
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
   document.getElementById('root')
 );
